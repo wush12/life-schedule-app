3697ac4cfbbaf180585f61b5ad4ac0521ce53486
吴世豪 <wushihao@wushihaodeMacBook-Pro.local>
2018-12-29 23:06:24 +0800

    清除appId

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -18,7 +18,7 @@
       <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页</a>
+    <a href="/pages/counter/main" class="counter">去往Vuex示</a>
   </div>
 </template>
 
